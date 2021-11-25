d76807c0b538e2da6afbdea77476bd9e1e2ac3de
Krzysiek <krzysztof.uminski1@gmail.com>
2021-11-25 16:36:40 +0100

    Added build version

---
+++ dist/precache-manifest.df202897411d1b9cc382bbc1423d32c0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee9e71f2c6d5510d425",
+    "url": "/Portfolio-page/css/app.a77a8f75.css"
+  },
+  {
+    "revision": "254f42ef1c16b6b2bdf2",
+    "url": "/Portfolio-page/css/chunk-vendors.ff650d32.css"
+  },
+  {
+    "revision": "9121ebbfafe7e9dea8400894afb62ced",
+    "url": "/Portfolio-page/favicon.png"
+  },
+  {
+    "revision": "8747f8a98af1f8bad839c25848e844ca",
+    "url": "/Portfolio-page/img/bootstrap2.8747f8a9.svg"
+  },
+  {
+    "revision": "2e84f08c2153395daca7e28c3550948a",
+    "url": "/Portfolio-page/img/css-file-format.2e84f08c.svg"
+  },
+  {
+    "revision": "5f33c82fa78ea53aeb8241346d4aaf5d",
+    "url": "/Portfolio-page/img/git.5f33c82f.svg"
+  },
+  {
+    "revision": "1edb0484dae3999451d2c1bac5c0b0b8",
+    "url": "/Portfolio-page/img/github.1edb0484.svg"
+  },
+  {
+    "revision": "bccf7c89017a8be8dcf179b3d5ea5bfd",
+    "url": "/Portfolio-page/img/gulp.bccf7c89.svg"
+  },
+  {
+    "revision": "492735d53c0dd1de0b6a512c1d02b514",
+    "url": "/Portfolio-page/img/html-file-extension-interface.492735d5.svg"
+  },
+  {
+    "revision": "675b94fd29439c56f09d1864a8206752",
+    "url": "/Portfolio-page/img/js.675b94fd.svg"
+  },
+  {
+    "revision": "0ab48fb9ce8947c305f71406ae8bb17c",
+    "url": "/Portfolio-page/img/linkedin.0ab48fb9.svg"
+  },
+  {
+    "revision": "e65db044be6e121387c711db6781cc82",
+    "url": "/Portfolio-page/img/meme_generator.e65db044.jpg"
+  },
+  {
+    "revision": "430e5efbe0b0b8aae993c5b5259e9bf6",
+    "url": "/Portfolio-page/img/nodejs-icon.430e5efb.svg"
+  },
+  {
+    "revision": "082613ae8203301e1db90c6aa7c81f06",
+    "url": "/Portfolio-page/img/notes-app.082613ae.jpg"
+  },
+  {
+    "revision": "708652b36bd4eeed4da3952803bafb53",
+    "url": "/Portfolio-page/img/phone-tablet-and-laptop.708652b3.svg"
+  },
+  {
+    "revision": "e9a0ac613c152c2287ee7d3b1afd9c2c",
+    "url": "/Portfolio-page/img/portfolio.e9a0ac61.jpg"
+  },
+  {
+    "revision": "1f580be3786a1a61b93518f6d6c56112",
+    "url": "/Portfolio-page/img/sass.1f580be3.svg"
+  },
+  {
+    "revision": "a7ac77116b373fd794de37fe9a385f85",
+    "url": "/Portfolio-page/img/vuejs.a7ac7711.svg"
+  },
+  {
+    "revision": "84321bfdc382845f150d0d3713b3ff3d",
+    "url": "/Portfolio-page/img/weather-app.84321bfd.jpg"
+  },
+  {
+    "revision": "f860ba0662d30009a60d2cad830ff56a",
+    "url": "/Portfolio-page/img/webpack.f860ba06.svg"
+  },
+  {
+    "revision": "bc58622b505f0e13f96a75b11c5aca56",
+    "url": "/Portfolio-page/img/wordpress.bc58622b.svg"
+  },
+  {
+    "revision": "af7d3d999a78593977f8307f3b096d93",
+    "url": "/Portfolio-page/index.html"
+  },
+  {
+    "revision": "eee9e71f2c6d5510d425",
+    "url": "/Portfolio-page/js/app.7cd1d3ef.js"
+  },
+  {
+    "revision": "254f42ef1c16b6b2bdf2",
+    "url": "/Portfolio-page/js/chunk-vendors.6b9bf32b.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/Portfolio-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Portfolio-page/robots.txt"
+  }
+]);
